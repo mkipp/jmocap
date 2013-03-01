692458a87944db38c179a282735cee8a82742e05
kipp <kipp@Michaels-MacBook-Air.local>
2013-03-01 10:22:13 +0100

    [no commit message]

--- bin/jmocap.bat
+++ bin/jmocap.bat
@@ -1,2 +1,2 @@
 cd ..
-java -classpath lib/j3dcore.jar;lib/j3dutils.jar;lib/vecmath.jar;lib/JGraphics3D.jar;dist/jmocap.jar de.dfki.embots.mocap.Main
+java -classpath lib/j3dcore.jar;lib/j3dutils.jar;lib/vecmath.jar;lib/JGraphics3D.jar;dist/jmocap.jar de.jmocap.Launcher
